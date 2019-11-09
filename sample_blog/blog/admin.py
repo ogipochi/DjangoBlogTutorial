89e6b3ebec3f520d108759b00f686ab08bbfa65a
ogipochi <pochi.0226.ogi@gmail.com>
2019-11-09 16:49:07 +0900

    customize admin site

--- sample_blog/blog/admin.py
+++ sample_blog/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "title", 
+        "author", 
+        "publish",
+        "status",)
+    list_filter = ("publish","status",)          # フィルターを作成
+    search_fields = ("title","body")             # 検索フォームの作成と検索対象の指定
+    date_hierarchy = "publish"                   # 指定したフィールドの日時ごとにページを分けて管理
+    ordering = ("publish","author","title")      # 一覧表示のデフォルトの並び順を指定
+
+
+admin.site.register(Article, ArticleAdmin)

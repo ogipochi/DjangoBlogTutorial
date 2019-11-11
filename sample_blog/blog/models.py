19aa5b73998c58b3c57589570dd96b4338c57a2f
ogipochi <pochi.0226.ogi@gmail.com>
2019-11-11 20:57:16 +0900

    my commit

--- sample_blog/blog/models.py
+++ sample_blog/blog/models.py
@@ -2,41 +2,43 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+
+class PublishedManager(models.Manager):
+    """
+    デフォルトで公開中のもののみを取得
+    """
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter("published")
+
 class Article(models.Model):
-    STATUS_CHOICES = (                                          # 公開状態の選択肢
+    STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
 
+    ###### マネージャー ####################################################
+    
+    objects = models.Manager()             # 見えていないが実際にはこれがある
+    published = PublishedManager()
+
+    ########################################################################
     ###### フィールド定義 ##################################################
 
-    title = models.CharField(max_length=250)                     # 記事タイトル
-    author = models.ForeignKey(User,                             # 記事筆者
+    title = models.CharField(max_length=250) 
+    author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_articles')
-    body = models.TextField()                                    # 記事本文
-    publish = models.DateTimeField(default=timezone.now)         # 公開日
-    created = models.DateTimeField(auto_now_add=True)            # 記事作成日
-    updated = models.DateTimeField(auto_now=True)                # 記事最終編集日
-    status = models.CharField(max_length=10,                     # 公開状態
+    body = models.TextField() 
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
     ########################################################################
     class Meta:
-        """
-        ModelクラスのMetaクラス。
-        記事の並び順やレコードでかぶりがあってはいけないフィールドなど
-        ここで指定できる
-        公式ドキュメント：
-            https://docs.djangoproject.com/ja/2.2/ref/models/options/
-        """
-        ordering = ('-publish',)                                 # 記事の並び順。新しいものから順に並ぶ。
+        ordering = ('-publish',) 
 
     def __str__(self):
-        """
-        モデルを文字列として扱う際何を表示するかを定義するメソッド
-        Djangoの管理サイトでの表示や
-        print()
-        で表示した時などはここが呼ばれる
-        """
         return self.title

8c96e805c3d9f3d2e45e684543c20d21301bd007
ogipochi <pochi.0226.ogi@gmail.com>
2019-11-14 01:55:55 +0900

    all

--- sample_blog/blog/views.py
+++ sample_blog/blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, Http404
+from .models import Article
 
-# Create your views here.
+
+
+def article_list(request):
+    """
+    記事の一覧を表示する
+    """
+    articles = Article.published.all()             # 公開中の記事を全て取得
+    return render(
+        request,
+        "blog/article/list.html",
+        {"articles":articles}
+    )
+
+def article_detail(request, id):
+    """
+    記事の詳細を表示する
+    """
+    article = get_object_or_404(Article, id=id)     # 記事を取得、取得できなかった時点で404を返す
+
+    return render(
+        request,
+        "blog/article/detail.html",
+        {"article":article}
+    )
+
+
+def sample_view(request, id):
+    try:
+        article = Article.objects.get(id=id)
+        return render(
+        request,
+        "blog/article/detail.html",
+        {"article":article})
+
+    except Article.DoesNotExist as e:
+        return Http404
